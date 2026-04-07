4eaaa83175c5ceec64aab7fa25c6b092c4286fff
Dilliya <dilusha.37.writer@gmail.com>
2026-04-07 19:03:23 +0400

    do lightbox

--- index.js
+++ index.js
@@ -5,3 +5,10 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modalWindow = document.getElementById('modal_window');
+const open_lightbox = document.getElementById('open_lightbox');
+const close_lightbox = document.getElementById('close_lightbox');
+
+open_lightbox.onclick = () => modalWindow.style.display = "flex";
+close_lightbox.onclick = () => modalWindow.style.display = "none";
