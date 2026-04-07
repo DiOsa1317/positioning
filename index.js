0899c61f25bc7c64f85e2db79fa15d7dfc983fef
Ilona Kazantseva <Ilona.Kazantseva@urfu.me>
2026-04-07 20:16:49 +0500

    progress bar

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+'use strict';
+
+const bar = document.querySelector('.progress-container');
+const labelBlack = document.querySelector('.label-black');
+const labelWhite = document.querySelector('.label-white');
+const interval = setInterval(frame,30);
+let width = 0;
+function frame() {
+    if (width >= 100){
+        clearInterval(interval);
+        return;
+    }
+    width++;
+    bar.style.setProperty('--progress', width + '%');
+    const text = `Loading... ${width + '%'}`;
+    labelBlack.textContent = text;
+    labelWhite.textContent = text;
+}
